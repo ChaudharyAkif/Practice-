fbbae88adda5b0265d716c268b71fe3a936045dd
M AKIF <makifhussainmn@gmail.com>
2023-02-11 21:48:25 +0500

    d

---
+++ app.js
@@ -0,0 +1,78 @@
+
+
+
+var age= prompt("what is your age","your age is" );
+
+  if (age<14)
+  {
+      console.log("Your age is si")
+  }
+else{
+
+    console.log("you are not allow")
+}
+  
+
+
+
+
+
+
+var  percent = +prompt("When is your age", "your age is");
+if( percent >=90 && percent <=100 )
+{
+alert( "you are A+ Gruate")
+
+
+}
+ else if( percent >=80 && percent <=90 )
+{
+alert( "you are A Gruate")
+
+
+}
+
+
+ else if ( percent >=70 && percent <=80 )
+{
+alert( "you are B Gruate")
+
+}
+
+
+ else if ( percent >=60 && percent <=70 )
+{
+alert( "you are C Gruate")
+
+
+}
+
+
+ else if( percent >=50 && percent <=60)
+{
+alert( "you are D Gruate")
+
+
+}
+
+
+
+ else if( percent >=30 && percent <=40 )
+{
+alert( "you are E Gruate")
+
+
+}
+
+ else if( percent >=0 && percent <=30 )
+{
+alert( "you are F Gruate")
+
+
+}
+else {
+ 
+ alert("you do not correct find percent")
+
+}
+
